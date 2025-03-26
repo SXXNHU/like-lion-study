c51fa67dc3b31d16fa6c3304767c78e8fea1e63d
SXXHU <dofqls0127@gmail.com>
2025-03-26 16:23:17 +0900

    [FEAT] 컨트롤러(생성,조회,수정,삭제) 구현

--- week_two/src/main/java/com/weektwo/week_two/controller/BoardController.java
+++ week_two/src/main/java/com/weektwo/week_two/controller/BoardController.java
@@ -8,16 +8,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/boards")
+@RequestMapping("/boards")
 public class BoardController {
 
     @Autowired
     private BoardService boardService;
 
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
     @PostMapping
     public ResponseEntity<BoardDTO> createBoard(@RequestBody BoardDTO boardDTO) {
         BoardDTO createdBoard = boardService.createBoard(boardDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdBoard);
+    }
+
+    @GetMapping("{boardId}")
+    public ResponseEntity<BoardDTO> getBoard(@PathVariable String boardId) {
+        BoardDTO boardDTO = boardService.getBoard(boardId);
+        if (boardDTO == null) {
+            return ResponseEntity.notFound().build(); //HTTP 404 반환
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(boardDTO); //HTTP 200 반환
     }
 
     @PutMapping("/{boardId}")
